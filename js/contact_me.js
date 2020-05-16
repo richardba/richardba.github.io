59490c77931a4d8caf820d712cf80ed63f145a70
Ricardo Barros <richardtrle@gmail.com>
2020-05-16 10:59:48 -0300

    Personal page

---
+++ js/contact_me.js
@@ -0,0 +1,59 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+          event.preventDefault();
+          var response = grecaptcha.getResponse();
+          var keyring = $.rot13;
+          var key = "6YqDQ_tHN"+//DFL5a1
+          "NNNNTfWwJ"+"pGYArBlslk"+"FxxxXc4hShy_";
+          if(response.length == 0) {
+          }
+          else
+          {
+            $.ajax({
+                type: "POST",
+                url: 'https://cors-anywhere.herokuapp.com/https://www.google.com/recaptcha/api/siteverify',
+                data: {"secret" : keyring(key), "response" : response, "remoteip":"localhost"},
+                contentType: 'application/x-www-form-urlencoded',
+                success: function(data) {
+                  contactForm.submit();
+                  contactForm.reset();
+                  $('#overlay').toggle();
+               }
+            });
+          }
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+
+    $( "#overlay button" ).click(function() {
+      $('#overlay').toggle();
+    });
+
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
+
+$("#contactForm").submit(function(e) {
+  e.preventDefault();
+});
+
+$(document).on('submit', '#contactForm', function(event){
+    event.preventDefault();
+});
